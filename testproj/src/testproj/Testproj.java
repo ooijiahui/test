36d94285b56bd68b68eb0ace2fe11cc04c1c5149
ooijiahui <126597154+ooijiahui@users.noreply.github.com>
2024-10-17 12:16:15 +0800

    Update Testproj.java

--- testproj/src/testproj/Testproj.java
+++ testproj/src/testproj/Testproj.java
@@ -16,7 +16,7 @@ public class Testproj {
     public static void main(String[] args) {
         // TODO code application logic here
         int i =0;
-        System.out.println(i);
+    
     }
     
 }
